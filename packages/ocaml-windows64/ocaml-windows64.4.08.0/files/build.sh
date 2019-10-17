677e7c6330c163a34a812ce968573433d1464c97
derui <derutakayu@gmail.com>
2019-10-17 21:19:59 +0900

    feat: more fast build in multicore

--- packages/ocaml-windows64/ocaml-windows64.4.08.0/files/build.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.08.0/files/build.sh
@@ -12,6 +12,4 @@ if grep "WITH_SPACETIME=true" Makefile.config >/dev/null 2>/dev/null; then
   echo "#define WITH_PROFINFO" >> runtime/caml/m.h
 fi
 
-make world opt \
-  compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlbytecomp.cmxa \
-  compilerlibs/ocamloptcomp.cmxa driver/main.cmx driver/optmain.cmx
+make -j world.opt

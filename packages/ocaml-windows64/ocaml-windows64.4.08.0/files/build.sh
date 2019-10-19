af0814edcdf875f1afb110f510bf3ee5fd94516b
derui <derutakayu@gmail.com>
2019-10-19 09:17:00 +0900

    fix: revert build optimizing

--- packages/ocaml-windows64/ocaml-windows64.4.08.0/files/build.sh
+++ packages/ocaml-windows64/ocaml-windows64.4.08.0/files/build.sh
@@ -12,4 +12,6 @@ if grep "WITH_SPACETIME=true" Makefile.config >/dev/null 2>/dev/null; then
   echo "#define WITH_PROFINFO" >> runtime/caml/m.h
 fi
 
-make -j world.opt
+make world opt \
+  compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlbytecomp.cmxa \
+  compilerlibs/ocamloptcomp.cmxa driver/main.cmx driver/optmain.cmx

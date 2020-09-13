2c1154054e036517d0d5784554bf4cec72593a14
derui <derutakayu@gmail.com>
2020-09-13 09:46:00 +0900

    feat: add ocaml-windows.4.09.1

---
+++ packages/ocaml-windows64/ocaml-windows64.4.09.1/files/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+HOST=$1
+
+./configure --host=$1
+cp -f Makefile.cross Makefile.config
+
+if grep "WITH_SPACETIME=true" Makefile.config >/dev/null 2>/dev/null; then
+  echo "#define WITH_SPACETIME" >> runtime/caml/m.h
+  echo "#define WITH_PROFINFO" >> runtime/caml/m.h
+fi
+
+CPUCOUNT=$(grep -c "^processor" /proc/cpuinfo)
+
+make -j ${CPUCOUNT} world
+make -j ${CPUCOUNT} opt
+make -j ${CPUCOUNT} \
+  compilerlibs/ocamlcommon.cmxa compilerlibs/ocamlbytecomp.cmxa \
+  compilerlibs/ocamloptcomp.cmxa driver/main.cmx driver/optmain.cmx
